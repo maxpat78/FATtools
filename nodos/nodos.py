7f419c8dc971dbffc42bc9f795c2720cfc60a025
maxpat78 <maxpat78@yahoo.it>
2022-02-03 18:13:11 +0100

    Update nodos.py
    Code adapted to Python3

--- nodos/nodos.py
+++ nodos/nodos.py
@@ -1,1 +1,1 @@
-print "Stringifying NODOS code (to put at offset 5Ah/78h)...\n", ''.join(['\\x%02X' % ord(c) for c in open('nodos','rb').read()])
+print ("Stringifying NODOS code (to put at offset 5Ah/78h)...\n", ''.join(['\\x%02X' % c for c in open('nodos','rb').read()]))
