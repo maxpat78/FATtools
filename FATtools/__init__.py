abaad3cc9cb427336fa6b27b53b3e61d7889d2a0
maxpat78 <maxpat78@yahoo.it>
2023-02-07 09:43:53 +0100

    Update __init__.py
    Updated Copyright and GitHub infos

--- FATtools/__init__.py
+++ FATtools/__init__.py
@@ -1,7 +1,7 @@
 # FATtools3
 from .version import *
 
-COPYRIGHT = '''Copyright (C)2012-2022, by maxpat78. GNU GPL v3 applies.
+COPYRIGHT = '''Copyright (C)2012-2023, by maxpat78. GNU GPL v3 applies.
 This free software manages (ex)FAT file systems and virtual disk images (VHD, VHDX, VDI, VMDK) WITH ABSOLUTELY NO WARRANTY!'''
 
-GITHUB = 'https://github.com/maxpat78/FATtools3'
+GITHUB = 'https://github.com/maxpat78/FATtools'
